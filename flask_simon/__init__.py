5c7e6b326123e385768edec37db594dcad27dc84
Andy Dirnberger <dirn@dirnonline.com>
2013-01-19 20:06:36 -0500

    Add docstrings

--- flask_simon/__init__.py
+++ flask_simon/__init__.py
@@ -6,11 +6,22 @@ __all__ = ('Simon', 'get_or_404')
 
 
 class Simon(object):
+    """Automatically creates a default connection for Simon models."""
+
     def __init__(self, app=None):
         if app is not None:
             self.init_app(app)
 
     def init_app(self, app):
+        """Initializes the Flask app for use with Simon.
+
+        This method will automatically be called if the app is passed
+        into ``__init__()``.
+
+        :param app: the Flask application.
+        :type app: :class:`flask.Flask`
+        """
+
         if 'simon' not in app.extensions:
             app.extensions['simon'] = {}
 
@@ -41,6 +52,22 @@ class Simon(object):
 
 
 def get_or_404(model, *qs, **fields):
+    """Finds and returns a single document, or raises a 404 exception.
+
+    This method will find a single document within the specified
+    model. If the specified query matches zero or multiple documents,
+    a ``404 Not Found`` exception will be raised.
+
+    :param model: the model class.
+    :type model: :class:`simon.Model`
+    :param \*qs: logical queries.
+    :type \*qs: :class:`simon.query.Q`
+    :param \*\*fields: keyword arguments specifying the query.
+    :type \*\*fields: kwargs
+
+    .. versionadded: 0.1.0
+    """
+
     try:
         return model.get(*qs, **fields)
     except (model.NoDocumentFound, model.MultipleDocumentsFound):
