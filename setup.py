8390714e0f2fcf2ac87ccb012973398e12c51f92
Andy Dirnberger <dirn@dirnonline.com>
2013-07-31 08:15:28 -0400

    Require a version of Simon that supports Python 3
    Now that both Simon and Flask-Simon support Python 3, it's a good idea
    to make sure that someone using both libraries is using versions of each
    that support Python 3.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     packages=['flask_simon'],
     package_data={'': ['LICENSE', 'README.rst']},
     include_package_data=True,
-    install_requires=['flask>=0.8', 'pymongo>=2.1', 'simon>=0.5'],
+    install_requires=['flask>=0.8', 'pymongo>=2.1', 'simon>=0.7'],
     tests_require=['coverage', 'mock', 'nose'],
     license=open('LICENSE').read(),
     classifiers=[
